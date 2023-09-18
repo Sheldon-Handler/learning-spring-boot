6a6ef6d6b861a097f1fe422660cebe9ce3564644
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2023-09-18 15:49:23 -0400

    Completed section 2

--- src/main/java/com/yourname/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/com/yourname/learningspringboot/LearningSpringBootApplication.java
@@ -2,6 +2,9 @@ package com.yourname.learningspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringBootApplication {
@@ -10,4 +13,33 @@ public class LearningSpringBootApplication {
 		SpringApplication.run(LearningSpringBootApplication.class, args);
 	}
 
+	@RestController
+	class Resource {
+
+		@RequestMapping(
+				method = RequestMethod.GET
+		)
+		Message getMessage() {
+			return new Message("Hello World");
+		}
+	}
+
+	class Message {
+		private final String message;
+
+		public Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public String toString() {
+			return "Message{" +
+					"message='" + message + '\'' +
+					'}';
+		}
+	}
 }
